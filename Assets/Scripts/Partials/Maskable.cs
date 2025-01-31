0d7703fee454f93df2e94fb057bae4c8fa82549f
Valerio Morelli <valeriomorelli50@gmail.com>
2025-01-31 12:22:24 +0100

    Add a chain with mask

---
+++ Assets/Scripts/Partials/Maskable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Partials
+{
+    public class Maskable : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<MeshRenderer>().material.renderQueue = 3002;
+        }
+    }
+}
