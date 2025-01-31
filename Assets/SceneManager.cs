a617e475502bc146b1cc96980e301b00efdb3b53
Valerio Morelli <valeriomorelli50@gmail.com>
2025-01-31 11:28:18 +0100

    Remove Unused

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    [SerializeField] [Range(1, 100)] private float skySpeed = 4.0f;
+    [SerializeField] [Range(.1f, 10)] private float skySpeed = .4f;
     private static readonly int Rotation = Shader.PropertyToID("_Rotation");
 
     private void Update () {
