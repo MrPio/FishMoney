e5258713563183fe34dfa8e865a73e051f39ec38
Valerio Morelli <valeriomorelli50@gmail.com>
2025-02-01 13:05:46 +0100

    Add sky initialization

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -1,11 +1,19 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SceneManager : MonoBehaviour
 {
     [SerializeField] [Range(.1f, 10)] private float skySpeed = .4f;
     private static readonly int Rotation = Shader.PropertyToID("_Rotation");
+    private float _startSkyboxRotation;
+
+    private void Start()
+    {
+        _startSkyboxRotation = Random.Range(0, 360);
+    }
 
     private void Update () {
-        RenderSettings.skybox.SetFloat(Rotation, Time.time * skySpeed);
+        RenderSettings.skybox.SetFloat(Rotation, _startSkyboxRotation+Time.time * skySpeed);
     }
 }
